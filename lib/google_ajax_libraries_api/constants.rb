725a96851e43dfd6ff4f2840b06b385010c22c1a
rpheath <rpheath@gmail.com>
2009-01-15 11:29:58 -0500

    now supports jQuery 1.3.0

--- lib/google_ajax_libraries_api/constants.rb
+++ lib/google_ajax_libraries_api/constants.rb
@@ -15,7 +15,7 @@ module RPH
       
       # used to maintain supported library versions
       SUPPORTED_VERSIONS = {
-        JQUERY        => ['1.2.3', '1.2.6'],
+        JQUERY        => ['1.2.3', '1.2.6', '1.3.0'],
         JQUERYUI      => ['1.5.2'],
         PROTOTYPE     => ['1.6.0.2'],
         SCRIPTACULOUS => ['1.8.1', '1.8.2'],
