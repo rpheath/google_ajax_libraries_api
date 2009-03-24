c72c4c9d1e69467716a3188d2d217c9a98a0fcfe
handcrafted <deploy@gethandcrafted.com>
2009-03-24 10:30:11 +0800

    Adding jQuery-UI 1.7.1
    Signed-off-by: Ryan Heath <rpheath@gmail.com>

--- lib/google_ajax_libraries_api/constants.rb
+++ lib/google_ajax_libraries_api/constants.rb
@@ -16,7 +16,7 @@ module RPH
       # used to maintain supported library versions
       SUPPORTED_VERSIONS = {
         JQUERY        => ['1.2.3', '1.2.6', '1.3.0', '1.3.1', '1.3.2'],
-        JQUERYUI      => ['1.5.2', '1.5.3', '1.7.0'],
+        JQUERYUI      => ['1.5.2', '1.5.3', '1.7.0', '1.7.1'],
         PROTOTYPE     => ['1.6.0.2', '1.6.0.3'],
         SCRIPTACULOUS => ['1.8.1', '1.8.2'],
         MOOTOOLS      => ['1.11', '1.2.1'],
